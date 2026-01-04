b8edb3b312348cda07231c6f5a74b2090aee200d
Baohang Li <guaguagua.baohang@gmail.com>
2026-01-04 17:46:43 -0600

    Implement Layer interface

---
+++ Layer.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+# Don't necessarily need to have an abstract base class; could simply rely on duck typing; mainly for practice/experimentation
+class Layer(ABC):
+    @abstractmethod
+    def forward(self, x):
+        pass
+
+    @abstractmethod
+    def backward(self, dx):
+        pass
